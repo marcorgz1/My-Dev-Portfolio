7eed3c030bd933edcbfeb0f7e0953201ca994f51
marcorgz1 <marco.valdemoro@gmail.com>
2025-04-03 14:00:17 +0200

    feat: try to add component from the Chakra UI components library

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,23 +1,28 @@
-import { Link } from 'react-router-dom'
+import { Card, Image, Button } from '@chakra-ui/react'
 
 export function ProjectCard ({ title, description, image, technologies, github, url }) {
     return (
-        <section className='project_card'>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <img src={image} alt="Project image" />
-            <div>
-            {
-                technologies.map((technology, index) => (
-                    <span key={index}>{technology}</span>
-                ))
-            }
-            </div>
-            <div className="project_buttons">
-                <Link className='project_button' to={github}>Github</Link>
-                <Link className='project_button' to={url}>Demo</Link>
-            </div>
-
-        </section>
+        <Card.Root>
+            <Image src={image} alt='Project image' />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Description>{description}</Card.Description>
+                <div>
+                {
+                    technologies.map((technology, index) => (
+                        <span key={index}>{technology}</span>
+                    ))
+                }
+                </div>
+            </Card.Body>
+            <Card.Footer>
+                <Button variant='solid'>
+                {github && <a href={github} target="_blank">Github</a>}
+                </Button>
+                <Button variant='solid'>
+                {url && <a href={url} target="_blank">Demo</a>}
+                </Button>
+            </Card.Footer>
+        </Card.Root>
     )
 }
