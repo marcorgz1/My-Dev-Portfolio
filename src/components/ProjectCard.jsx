7149c2123168900c917140a8e54a5aceeb4e83e6
marcorgz1 <marco.valdemoro@gmail.com>
2025-04-08 12:00:52 +0200

    feat: try to implement Material UI components

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,28 +1,43 @@
-import { Card, Image, Button } from '@chakra-ui/react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography';
 
-export function ProjectCard ({ title, description, image, technologies, github, url }) {
+export function ProjectCard({ title, description, image, technologies, github, url }) {
     return (
-        <Card.Root>
-            <Image src={image} alt='Project image' />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Description>{description}</Card.Description>
-                <div>
-                {
-                    technologies.map((technology, index) => (
-                        <span key={index}>{technology}</span>
-                    ))
-                }
-                </div>
-            </Card.Body>
-            <Card.Footer>
-                <Button variant='solid'>
-                {github && <a href={github} target="_blank">Github</a>}
+        <Card variant='outlined'>
+            <CardHeader>
+                <Typography variant='h2'>{title}</Typography>
+            </CardHeader>
+                <CardMedia 
+                    component="img"
+                    height="194"
+                    image={image}
+                    alt='First project image'
+                    />
+                <CardContent>                
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    {description}
+                    </Typography>
+                    <Typography>
+                    {technologies.map((technology) => (
+                        <span>{technology}</span>
+                    ))}
+                    </Typography>
+                </CardContent>
+            <CardActions>
+                <Button size="small">
+                    <a href={github}>GitHub</a>
                 </Button>
-                <Button variant='solid'>
-                {url && <a href={url} target="_blank">Demo</a>}
+                <Button size="small">
+                    <a href={url}>Demo</a>
                 </Button>
-            </Card.Footer>
-        </Card.Root>
+            </CardActions>
+        </Card>
     )
 }
