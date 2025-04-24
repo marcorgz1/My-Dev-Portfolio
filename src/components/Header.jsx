9699451bbf2ea1772ed6231f34deb1b623569070
marcorgz1 <marco.valdemoro@gmail.com>
2025-04-24 15:50:29 +0200

    feat: update content of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,14 @@
 import { Navbar } from './Navbar'
-import UseAnimations from 'react-useanimations';
-import github from 'react-useanimations/lib/github';
-import linkedin from 'react-useanimations/lib/linkedin';
 import '../css/Header.css'
 
 export function Header () {
     return (
         <main className='header'>
-            <div></div>     
+            <div>
+                <span>Marco</span>    
+            </div>     
             <Navbar />
-            <div className='social_buttons'>
-                <UseAnimations animation={github} size={32} strokeColor='white' />
-                <UseAnimations animation={linkedin} size={32} strokeColor='white' />
-            </div>
+            <div></div>       
         </main>
     )
 }
